5fa388e422167470b8aae5fb6e0b028729f6000b
Connor <up857678@myport.ac.uk>
2018-04-13 14:43:47 +0100

    Finished Level 1 Environment
    I have finished the environment for the first level ready for Ben to implement UI, AI, Item pick up and doors. (Assuming that is still the plan). Alterations to the environment and models may be needed later on.
    -Connor

---
+++ CT4TDEMO/Assets/PlayerController.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour {
+
+	public float speed = 5.0F;
+	private Rigidbody rb;
+	public LayerMask groundLayers;
+	public float jumpForce = 10.0f;
+	public CapsuleCollider col;
+
+	void Start () {
+		rb = GetComponent<Rigidbody> ();
+		col = GetComponent<CapsuleCollider> ();
+		Cursor.lockState = CursorLockMode.Locked;
+	}
+
+	void Update () {
+		float translation = Input.GetAxis ("Vertical") * speed;
+		float straffe = Input.GetAxis ("Horizontal") * speed;
+		translation *= Time.deltaTime;
+		straffe *= Time.deltaTime;
+
+		transform.Translate (straffe, 0, translation);
+
+		if (IsGrounded() && Input.GetKeyDown (KeyCode.Space))
+		{
+			rb.AddForce (Vector3.up * jumpForce, ForceMode.Impulse);
+		}
+
+		if (Input.GetKeyDown ("escape"))
+			Cursor.lockState = CursorLockMode.None;
+	}
+
+	private bool IsGrounded()
+	{
+		return Physics.CheckCapsule (col.bounds.center, new Vector3 (col.bounds.center.x, col.bounds.min.y, col.bounds.center.z), col.radius * .9f, groundLayers);
+	}
+}
